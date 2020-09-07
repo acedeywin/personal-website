0112ac7d0b040c374a74e0a7984ea62b7298afbc
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-09-07 21:46:52 +0100

    ft-url # removal

--- app/scripts/web.js
+++ app/scripts/web.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 
-var eventsApp = angular.module('eventsApp', ['ngRoute']);
+var eventsApp = angular.module("eventsApp", ["ngRoute"]);
 
-eventsApp.config(function($routeProvider){
-    $routeProvider
-        .when('/homepage',{
-            templateUrl: 'homepage.html',
-            controller: 'maincontroller'
-        })
-        .when('/gallery',{
-            templateUrl: 'gallery.html',
-            controller: 'maincontroller'
-        })
-        .when('/about',{
-            templateUrl: 'about.html',
-            controller: 'maincontroller'
-        })
-        .when('/portfolio',{
-            templateUrl: 'portfolio.html',
-            controller: 'maincontroller'
-        })
-        .when('/contact',{
-            templateUrl: 'contact.html',
-            controller: 'maincontroller'
-        })
-        .otherwise({redirectTo: '/homepage'})
-})
+eventsApp.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/homepage", {
+      templateUrl: "homepage.html",
+      controller: "maincontroller",
+    })
+    .when("/gallery", {
+      templateUrl: "gallery.html",
+      controller: "maincontroller",
+    })
+    .when("/about", {
+      templateUrl: "about.html",
+      controller: "maincontroller",
+    })
+    .when("/portfolio", {
+      templateUrl: "portfolio.html",
+      controller: "maincontroller",
+    })
+    .when("/contact", {
+      templateUrl: "contact.html",
+      controller: "maincontroller",
+    })
+    .otherwise({ redirectTo: "/homepage" });
+  $locationProvider.html5Mode(true);
+});
--- web-server/app.js
+++ web-server/app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 var app = express();
-var rootPath = path.normalize(__dirname + '/../');
+var rootPath = path.normalize(__dirname + "/../");
 
-app.use(express.static(rootPath + '/app'));
+app.use(express.static(rootPath + "/app"));
 
 let port = process.env.PORT;
-if (port == null || port == '') {
+if (port == null || port == "") {
   port = 3001;
 }
 app.listen(port);
-console.log('Listening on port 3001...');
+console.log("Listening on port 3001...");
